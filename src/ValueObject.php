75a11bf09391d735daf98b939aa0b24d7e989c3b
Bruce <bruce@mediagone.fr>
2020-12-20 00:39:26 +0100

    Add "ValueObject" interface

---
+++ src/ValueObject.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Mediagone\Common\Types;
+
+use JsonSerializable;
+
+
+interface ValueObject extends JsonSerializable
+{
+    /**
+     * Returns whether the specified value is valid for the given object.
+     */
+    public static function isValueValid($value) : bool;
+    
+    public function __toString() : string;
+}
